73610c726440571e00ee789c2ee4272292773148
coleyon <coleyon784@gmail.com>
2019-08-08 01:14:57 +0900

    added cron service starting

--- containers/put_glpi.sh
+++ containers/put_glpi.sh
@@ -12,31 +12,9 @@ SRC_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/tags/
 TAR_GLPI=$(basename ${SRC_GLPI})
 FOLDER_GLPI=glpi/
 FOLDER_WEB=/var/www/html/
-FOLDER_PATCHES=/tmp/patches
 
 # Downloading and extracting sources of GLPI
-if [ "$(ls ${FOLDER_WEB}${FOLDER_GLPI})" ];
-then
-	echo "GLPI is already installed"
-else
-	wget -P ${FOLDER_WEB} ${SRC_GLPI}
-	tar -xzf ${FOLDER_WEB}${TAR_GLPI} -C ${FOLDER_WEB}
-	rm -Rf ${FOLDER_WEB}${TAR_GLPI}
-	chown -R www-data:www-data ${FOLDER_WEB}${FOLDER_GLPI}
-fi
-
-# add truetype ipafont for printing japanese pdf
-/var/www/html/glpi/vendor/tecnickcom/tcpdf/tools/tcpdf_addfont.php -i /usr/share/fonts/truetype/fonts-japanese-gothic.ttf
-/var/www/html/glpi/vendor/tecnickcom/tcpdf/tools/tcpdf_addfont.php -i /usr/share/fonts/truetype/fonts-japanese-mincho.ttf
-echo "IPA True Type font installed for GLPI."
-
-# apply patches
-if [ "$(ls ${FOLDER_PATCHES}/*.patch 2>/dev/null)" ];
-then
-	cd $FOLDER_WEB$FOLDER_GLPI
-	for patchfile in `find $FOLDER_PATCHES -maxdepth 1 -type f -name "*.patch"`; do
-		patch -u -t -p1 < $patchfile
-	done
-else
-	echo "Patching step skipped."
-fi
+wget -P ${FOLDER_WEB} ${SRC_GLPI}
+tar -xzf ${FOLDER_WEB}${TAR_GLPI} -C ${FOLDER_WEB}
+rm -Rf ${FOLDER_WEB}${TAR_GLPI}
+chown -R www-data:www-data ${FOLDER_WEB}${FOLDER_GLPI}
